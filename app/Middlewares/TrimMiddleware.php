b4a0b408924c37a60771cebf06a99a65c9a9cbd6
SixSad <viktor-viktor-82@inbox.ru>
2022-02-08 18:50:52 +0700

    add packet

--- app/Middlewares/TrimMiddleware.php
+++ app/Middlewares/TrimMiddleware.php
@@ -3,14 +3,17 @@
 namespace Middlewares;
 
 use Src\Request;
+use function Collect\collection;
 
 class TrimMiddleware
 {
     public function handle(Request $request)
     {
-        foreach ($request->all() as $key => $value) {
-            $request->set($key, is_string($value) ? trim($value) : $value);
-        }
+        collection($request->all())
+            ->each(function ($value, $key, $request) {
+                $request->set($key, is_string($value) ? trim($value) : $value);
+            }, $request);
+
         return $request;
     }
 }
